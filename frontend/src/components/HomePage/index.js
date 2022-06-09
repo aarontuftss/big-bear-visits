e3320a79156b12fd6315241e8570a962a8af808e
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-06-08 23:07:54 -0400

    more styline

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import bg from './bg-1.jpeg'
-import bgv from './BigBearSpringBanner.mp4'
 
 import './HomePage.css';
 
@@ -22,6 +20,32 @@ function HomePage() {
                     <input type='number' placeholder='# of guests'></input>
                 </form>
                 <h2>Plan your next trip to beautiful Big Bear Lake, California</h2>
+
+            </div>
+            <div className='homeCardContainer'>
+                <div className='homeCard'>
+                    <img  src={''} alt=''></img>
+                    <p>test</p>
+                    <p>test</p>
+                </div>
+
+                <div className='homeCard'>
+                    <img src={''} alt=''></img>
+                    <p>test</p>
+                    <p>test</p>
+                </div>
+
+                <div className='homeCard'>
+                    <img src={''} alt=''></img>
+                    <p>test</p>
+                    <p>test</p>
+                </div>
+
+                <div className='homeCard'>
+                    <img src={''} alt=''></img>
+                    <p>test</p>
+                    <p>test</p>
+                </div>
 
             </div>
 
