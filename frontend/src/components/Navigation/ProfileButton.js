524c649e411dbd0a0926170c1268e57d8334a507
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-06-08 20:48:09 -0400

    navbar

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,27 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import {NavLink} from 'react-router-dom'
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
-    };
-
-    useEffect(() => {
-        if (!showMenu) return;
-
-        const closeMenu = () => {
-            setShowMenu(false);
-        };
-
-        document.addEventListener('click', closeMenu);
-
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
 
     const logout = (e) => {
         e.preventDefault();
@@ -30,18 +14,11 @@ function ProfileButton({ user }) {
 
     return (
         <>
-            <button onClick={openMenu}>
-                <i className="fas fa-user-circle" />
-            </button>
-            {showMenu && (
-                <ul className="profile-dropdown">
-                    <li>{user.username}</li>
-                    <li>{user.email}</li>
-                    <li>
-                        <button onClick={logout}>Log Out</button>
-                    </li>
-                </ul>
-            )}
+            <div className="userBHold">
+                <NavLink to={`/properties/new`}>Upload Home</NavLink>
+                <NavLink to={`/users/${user.id}`}>Profile</NavLink>
+                <button onClick={logout}>Log Out</button>
+            </div>
         </>
     );
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from './logooo.png'
+import logo1 from './logo1.png'
+import arrow from './arrow.png'
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
@@ -15,20 +18,37 @@ function Navigation({ isLoaded }) {
     } else {
         sessionLinks = (
             <>
-                <NavLink to="/login">Log In</NavLink>
-                <NavLink to="/signup">Sign Up</NavLink>
+                <NavLink to="/login" className='authB'>Log In</NavLink>
+                <NavLink to="/signup" className='authB'>Sign Up</NavLink>
             </>
         );
     }
 
     return (
-        <ul>
-            <li>
-                <NavLink exact to="/">Home</NavLink>
+        <>
+        <div className='navBar'>
+            <div className='logoHold navS'>
+                <NavLink exact to="/"><img src={logo1} alt='' className='logo1'></img></NavLink>
+                <NavLink exact to="/"><img src={logo} alt='' className='logo'></img></NavLink>
+            </div>
+
+            <div className='mid'>
+                <NavLink to={`/search`}>
+                <div className='centerBook'>
+                    <p>Check Availability</p>
+                    <img src={arrow} alt='' className='arrow1'></img>
+                </div>
+                </NavLink>
+            </div>
+
+            <div className='navS'>
                 {isLoaded && sessionLinks}
-            </li>
-        </ul>
+            </div>
+            
+        </div>
+        </>
     );
 }
 
 export default Navigation;
+
